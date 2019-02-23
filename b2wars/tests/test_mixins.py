40d61e88c95960c5fb45b2d33a2c42f2620cf857
Abe Kroenem <paulo.alfredo.barros@gmail.com>
2019-02-23 15:58:46 -0400

    tests(mixins): Add check for mixin is abstract model

--- b2wars/tests/test_mixins.py
+++ b2wars/tests/test_mixins.py
@@ -1,12 +1,19 @@
 from datetime import datetime
 from django.test import TestCase
 
+from b2wars.apps.core.mixins.models import TimeStampedMixin
 from b2wars.tests.models import TimeStampedMock
 
-class MixinsTestCase(TestCase):
+class TimeStampedMixinTestCase(TestCase):
 
     def setUp(self):
         self.tsm_instance = TimeStampedMock.objects.create()
+
+    def test_is_abstract(self):
+        """ Check if TimeStampedMixin is a abstract Model """
+        # pylint: disable=protected-access
+        self.assertTrue(TimeStampedMixin._meta.abstract)
+
 
     def test_model_has_created_and_updated_fields(self):
         """
@@ -15,8 +22,8 @@ class MixinsTestCase(TestCase):
         self.assertTrue(hasattr(TimeStampedMock, 'created_at'))
         self.assertTrue(hasattr(TimeStampedMock, 'updated_at'))
 
-        self.assertEqual(type(self.tsm_instance.created_at), datetime)
-        self.assertEqual(type(self.tsm_instance.updated_at), datetime)
+        self.assertIs(type(self.tsm_instance.created_at), datetime)
+        self.assertIs(type(self.tsm_instance.updated_at), datetime)
 
 
     def test_created_at_value_on_add(self):
@@ -39,6 +46,7 @@ class MixinsTestCase(TestCase):
             self.tsm_instance.updated_at,
             self.tsm_instance.created_at
         )
+
 
     def tearDown(self):
         self.tsm_instance.delete()
