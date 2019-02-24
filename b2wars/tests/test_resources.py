4caf95f97217d84ada996d9c8b4b3bb94770e1c1
Abe Kroenem <paulo.alfredo.barros@gmail.com>
2019-02-24 10:05:46 -0400

    tests(planets): Add simple Test Case for Planets CRUD

---
+++ b2wars/tests/test_resources.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from b2wars.apps.planets.models import Planet
+
+class PlanetsResourceTestCase(TestCase):
+
+
+    def test_planet_resource_is_avaliable(self):
+        pass
+
+
+    def test_get_planets_list(self):
+        pass
+
+
+    def test_get_planet_object(self):
+        pass
+
+
+    def test_create_planet(self):
+        pass
+
+
+    def test_full_update_planet(self):
+        pass
+
+
+    def test_partial_update_planet(self):
+        pass
+
+
+    def test_delete_a_planet(self):
+        pass
