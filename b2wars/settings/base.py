223504abdff69d09e9b7f60502a62cab0bbf3bdc
abekroenem <paulo.alfredo.barros@gmail.com>
2019-02-25 13:07:38 -0400

    fix: STATICFILES_DIRS as tuple

--- b2wars/settings/base.py
+++ b2wars/settings/base.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Whitenoise configuration FileStorage
