b0715bc082f1dba6cad63e531ac5a6ed27fdeb35
abekroenem <paulo.alfredo.barros@gmail.com>
2019-02-25 11:34:01 -0400

    feat(db): Add explicity connection params to mLab on Heroku

--- b2wars/settings/heroku.py
+++ b2wars/settings/heroku.py
@@ -6,13 +6,24 @@ from b2wars.settings.dev import *
 # MongoDB container connection
 # see docker-compose.yml
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': os.environ.get('B2WARS_HEROKU_DB_NAME'),
+#         'HOST': os.environ.get('B2WARS_HEROKU_DB_HOST'),
+#         'USER': os.environ.get('B2WARS_HEROKU_DB_USER'),
+#         'PORT': int(os.environ.get('B2WARS_HEROKU_DB_PORT')),
+#         'PASSWORD': os.environ.get('B2WARS_HEROKU_DB_PASSWORD'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
-        'NAME': os.environ.get('B2WARS_HEROKU_DB_NAME'),
-        'HOST': os.environ.get('B2WARS_HEROKU_DB_HOST'),
-        'USER': os.environ.get('B2WARS_HEROKU_DB_USER'),
-        'PORT': int(os.environ.get('B2WARS_HEROKU_DB_PORT')),
-        'PASSWORD': os.environ.get('B2WARS_HEROKU_DB_PASSWORD'),
+        'NAME': 'heroku_0lhl3p2z',
+        'HOST': 'ds149875.mlab.com',
+        'USER': 'heroku_0lhl3p2z',
+        'PORT': 49875,
+        'PASSWORD': 'klqct85ltut5vi6v4n9jomd7hk',
     }
 }
